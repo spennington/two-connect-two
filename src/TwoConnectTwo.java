61662196ac101c84c1dfbe99598bbb3392009f86
pennington.15@gmail.com <pennington.15@gmail.com@4dab4299-bd51-09d2-4e9f-d8f47a67b301>
2011-05-15 18:33:25 +0000

    Handle case where board is empty
    git-svn-id: https://julie-steve-ceng.googlecode.com/svn/trunk@15 4dab4299-bd51-09d2-4e9f-d8f47a67b301

--- src/TwoConnectTwo.java
+++ src/TwoConnectTwo.java
@@ -15,7 +15,11 @@ public class TwoConnectTwo {
 		GameState gameState = IO.parseGameState();
 		Board board = IO.parseBoard(gameState.numCols, gameState.numRows);
 		IO.close();
-		// Input assumes first column is 1 we use first column as 0, so subtract 1
-		System.out.println(WinChecker.isWin(board, gameState.lastCol - 1));
+		if(board.isEmpty()) {
+			System.out.println("0");
+		} else {
+			// Input assumes first column is 1 we use first column as 0, so subtract 1
+			System.out.println(WinChecker.isWin(board, gameState.lastCol - 1));
+		}
 	}
 }
