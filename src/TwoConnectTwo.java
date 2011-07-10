697f2217de2488080b77542bee8905ac7531123f
pennington.15@gmail.com <pennington.15@gmail.com@4dab4299-bd51-09d2-4e9f-d8f47a67b301>
2011-07-10 04:05:22 +0000

    Progressive deepening Force opponent overtime if they have passed 10mins
    git-svn-id: https://julie-steve-ceng.googlecode.com/svn/trunk@59 4dab4299-bd51-09d2-4e9f-d8f47a67b301

--- src/TwoConnectTwo.java
+++ src/TwoConnectTwo.java
@@ -6,6 +6,11 @@ public class TwoConnectTwo {
 
 	private static final int BRUTE_FORCE_DEPTH = 17;
 	private static final int START_DEPTH = 6;
+	private static final int MILLIS_IN_MIN = 60000;
+	private static final int MILLIS_IN_SEC = 1000;
+	private static final int IDEAL_TIME = 12 * MILLIS_IN_SEC;
+	private static final int MAX_TIME = 3 * MILLIS_IN_MIN;
+	private static final int TIME_BUFFER = 5 * MILLIS_IN_SEC;
 	
 	/**
 	 * Main entry point into the application
@@ -13,19 +18,53 @@ public class TwoConnectTwo {
 	 * 		command line arguments
 	 */
 	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
 		IO.init(System.in);
 		GameState gameState = IO.parseGameState();
 		Board board = IO.parseBoard(gameState.numCols, gameState.numRows);
 		IO.close();
 		
-		Move move;
+		boolean playerOne = (board.getPieceCount() & 1) == 0;
+		int myTime;
+		int theirTime;
+		
+		if(playerOne) {
+			myTime = gameState.playerOneTime;
+			theirTime = gameState.gameTime - gameState.playerOneTime;
+		} else {
+			theirTime = gameState.playerOneTime;
+			myTime = gameState.gameTime - gameState.playerOneTime;
+		}
+		
+		boolean takeMax = theirTime >= 10*MILLIS_IN_MIN;
+		int timeAllowed = takeMax ? MAX_TIME - TIME_BUFFER : IDEAL_TIME;
+		
+		Move move = null;
 		int piecesRemaining = board.getWidth() * board.getHeight() - board.getPieceCount();
 		if(piecesRemaining <= BRUTE_FORCE_DEPTH) {
 			Evaluator evaluator = WinEvaluator.getInstance();
 			move = AI.minMax(board, piecesRemaining, evaluator);
 		} else {
-			Evaluator evaluator = ChainEvaluator.getInstance();
-			move = AI.minMax(board, START_DEPTH, evaluator);
+			int depth = START_DEPTH;
+			long taken = System.currentTimeMillis() - startTime;
+			while(taken < timeAllowed){
+				Evaluator evaluator = ChainEvaluator.getInstance();
+				move = AI.minMax(board, depth, evaluator);
+				taken = System.currentTimeMillis() - startTime;
+				if(taken * 20 < timeAllowed) {
+					depth++;
+					System.err.println("calyspo+");
+				}
+			}
+		}
+		
+		if(takeMax) {
+			try {
+				System.err.println("Sleeping to win");
+				Thread.sleep(MAX_TIME - (System.currentTimeMillis() - startTime) - TIME_BUFFER);
+			} catch (InterruptedException e) {
+				//do nothing
+			}
 		}
 		
 		System.out.println("(" + (move.column + 1)+ ", " + IO.pieceMap[move.piece] + ")");
