830717c63c4cc4d431de3e0b3604b4a4c8023718
pennington.15@gmail.com <pennington.15@gmail.com@4dab4299-bd51-09d2-4e9f-d8f47a67b301>
2011-07-11 04:57:29 +0000

    Add win property to moves
    git-svn-id: https://julie-steve-ceng.googlecode.com/svn/trunk@67 4dab4299-bd51-09d2-4e9f-d8f47a67b301

--- src/Move.java
+++ src/Move.java
@@ -6,13 +6,19 @@ public class Move implements Cloneable {
 	public int column;
 	public int score;
 	public byte piece;
+	public boolean isWin = false;
 	
 	public Move() {}
 	
 	public Move(int column, int score, byte piece) {
+		this(column, score, piece, false);
+	}
+	
+	public Move(int column, int score, byte piece, boolean isWin) {
 		this.column = column;
 		this.score = score;
 		this.piece = piece;
+		this.isWin = isWin;
 	}
 	
 	/* (non-Javadoc)
