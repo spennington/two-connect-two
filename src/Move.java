b77b750e9503df61de62d331ada37b9223cadff6
pennington.15@gmail.com <pennington.15@gmail.com@4dab4299-bd51-09d2-4e9f-d8f47a67b301>
2011-07-12 15:22:34 +0000

    Add depth and win flag to moves
    git-svn-id: https://julie-steve-ceng.googlecode.com/svn/trunk@74 4dab4299-bd51-09d2-4e9f-d8f47a67b301

--- src/Move.java
+++ src/Move.java
@@ -6,19 +6,29 @@ public class Move implements Cloneable {
 	public int column;
 	public int score;
 	public byte piece;
-	public boolean isWin = false;
+	public int depth;
+	public boolean isWin;
 	
 	public Move() {}
 	
 	public Move(int column, int score, byte piece) {
-		this(column, score, piece, false);
+		this(column, score, piece, 0, false);
 	}
 	
-	public Move(int column, int score, byte piece, boolean isWin) {
+	public Move(int column, int score, byte piece, int depth, boolean isWin) {
 		this.column = column;
 		this.score = score;
 		this.piece = piece;
+		this.depth = depth;
 		this.isWin = isWin;
+	}
+	
+	public void copy(Move move) {
+		this.column = move.column;
+		this.score = move.score;
+		this.piece = move.piece;
+		this.depth = move.depth;
+		this.isWin = move.isWin;
 	}
 	
 	/* (non-Javadoc)
@@ -26,6 +36,7 @@ public class Move implements Cloneable {
 	 */
 	@Override
 	protected Move clone() {
-		return new Move(this.column, this.score, this.piece);
+		return new Move(this.column, this.score, this.piece, this.depth, this.isWin);
 	}
+	
 }
