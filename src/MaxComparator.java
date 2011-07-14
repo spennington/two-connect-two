11bead47af66843de8bf4de193fa49b5d475b30c
pennington.15@gmail.com <pennington.15@gmail.com@4dab4299-bd51-09d2-4e9f-d8f47a67b301>
2011-07-14 05:37:03 +0000

    Just compare using scores due to pruning issues
    git-svn-id: https://julie-steve-ceng.googlecode.com/svn/trunk@86 4dab4299-bd51-09d2-4e9f-d8f47a67b301

--- src/MaxComparator.java
+++ src/MaxComparator.java
@@ -15,17 +15,17 @@ public class MaxComparator implements Comparator<Move> {
 	 */
 	@Override
 	public int compare(Move m1, Move m2) {
-		if(m1.isWin && m2.isWin && m1.score > 1 && m2.score > 1) {
+		//if(m1.isWin && m2.isWin && m1.score > 1 && m2.score > 1) {
 			//Red win, so we want the lowest depth
-			return m2.depth - m1.depth;
-		} else if(m1.isWin && m2.isWin && m1.score < 1 && m2.score < 1) {
+		//	return m2.depth - m1.depth;
+		//} else if(m1.isWin && m2.isWin && m1.score < 1 && m2.score < 1) {
 			//Blue win, so we want the highest depth
-			return m1.depth - m2.depth;
-		} else if(m1.score != m2.score) {
+		//	return m1.depth - m2.depth;
+		//} else 
+		if(m1.score != m2.score) {
 			return m1.score > m2.score ? 1 : -1;
 		} else {
-			//If all things are equal, take the closest
-			return m2.depth - m1.depth;
+			return 0;
 		}
 	}
 
--- src/MinComparator.java
+++ src/MinComparator.java
@@ -15,15 +15,21 @@ public class MinComparator implements Comparator<Move> {
 	 */
 	@Override
 	public int compare(Move m1, Move m2) {
-		if(m1.isWin && m2.isWin && m1.score > 1 && m2.score > 1) {
+		//if(m1.isWin && m2.isWin && m1.score > 1 && m2.score > 1) {
 			//Red win, so we want the highest depth
 			//but we are selecting MIN
-			return m2.depth - m1.depth;
-		} else if(m1.score != m2.score) {
+			//return m2.depth - m1.depth;
+		//} else if(m1.score != m2.score) {
+		//	return m1.score > m2.score ? 1 : -1;
+		//} else {
+			//If all things are equal, take the closest
+		//	return m1.depth - m2.depth;
+		//}
+		
+		if(m1.score != m2.score) {
 			return m1.score > m2.score ? 1 : -1;
 		} else {
-			//If all things are equal, take the closest
-			return m1.depth - m2.depth;
+			return 0;
 		}
 	}
 
