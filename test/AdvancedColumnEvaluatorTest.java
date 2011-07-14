3d6b39d42794f339e7e27f58bdfab3def0177275
pennington.15@gmail.com <pennington.15@gmail.com@4dab4299-bd51-09d2-4e9f-d8f47a67b301>
2011-07-14 05:39:19 +0000

    Add some more heuristic tests
    git-svn-id: https://julie-steve-ceng.googlecode.com/svn/trunk@89 4dab4299-bd51-09d2-4e9f-d8f47a67b301

--- test/AdvancedColumnEvaluatorTest.java
+++ test/AdvancedColumnEvaluatorTest.java
@@ -16,13 +16,13 @@ public class AdvancedColumnEvaluatorTest {
 	@Test
 	public void testEvaluate() {
 		Board board = TestUtils.generateBoard("b,r,b,r,b,r,s,s,r,r,b,s,s,s,s,s,g,b,r,b,r,b,r,r,b,r,r,b,r,r,g,b,b,b,b,r,b,r,r,b,r,g,b,r,b,s,s,s,r,b,r,b,g,b,r,r,s,s,s,s,s,s,s,s,b,r,r,b,r,g,r,b,r,b,r,b,r,b,s,s", 10, 8);
-		assertEquals(9, advancedColumnEvaluator.evaluate(board, 0, Board.BLUE));
+		assertEquals(3520, advancedColumnEvaluator.evaluate(board, 0, Board.BLUE));
 	}
 	
 	@Test
 	public void testOneWin() {
 		Board board = TestUtils.generateBoard("b,r,b,r,b,r,b,r,s,s,s,s,s,s,s,s,g,b,r,s,s,s,s,s,r,r,b,r,g,r,b,r,b,r,b,r,b,r,b,r,b,b,r,b,b,r,b,r,r,b,r,b,r,b,r,b,r,s,s,s,s,s,s,s,b,r,b,r,b,r,b,r,s,s,s,s,s,s,s,s", 10, 8);
-		assertEquals(-50, advancedColumnEvaluator.evaluate(board, 0, Board.BLUE));
+		assertEquals(-500, advancedColumnEvaluator.evaluate(board, 0, Board.BLUE));
 	}
 	
 	@Test
@@ -34,7 +34,29 @@ public class AdvancedColumnEvaluatorTest {
 	@Test
 	public void redDoubleWin() {
 		Board board = TestUtils.generateBoard("s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,b,b,s,s,s,s,s,s,b,r,b,r,b,r,r,s,r,b,r,r,b,b,r,b,g,g,b,r,g,r,b,g,b,r,r,b,r,s,s,s,r,b,r,b,r,s,s,s,g,s,s,s,s,s,s,s", 10, 8);
-		assertEquals(-100000, advancedColumnEvaluator.evaluate(board, 0, Board.BLUE));
+		assertEquals(500000, advancedColumnEvaluator.evaluate(board, 0, Board.BLUE));
+	}
+	
+	@Test public void testWinNowBlue() {
+		Board board = new Board(10, 8);
+		board.drop(0, Board.BLUE);
+		board.drop(1, Board.BLUE);
+		board.drop(2, Board.GREEN);
+		assertEquals(-50000000, advancedColumnEvaluator.evaluate(board, 0, Board.BLUE));
+	}
+	
+	@Test public void testWinNowRed() {
+		Board board = new Board(10, 8);
+		board.drop(0, Board.RED);
+		board.drop(1, Board.BLUE);
+		board.drop(1, Board.RED);
+		board.drop(2, Board.BLUE);
+		board.drop(2, Board.RED);
+		board.drop(2, Board.GREEN);
+		board.drop(3, Board.RED);
+		board.drop(3, Board.BLUE);
+		board.drop(3, Board.RED);
+		assertEquals(50000000, advancedColumnEvaluator.evaluate(board, 0, Board.RED));
 	}
 
 }
